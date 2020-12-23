00bdbd43eaa19611f5f04239aa8114d3defcfcb4
ilhamsurya <ilhamsurya.m@gmail.com>
2020-12-23 11:36:57 +0700

    baca README

---
+++ app/backend/database/conn.py
@@ -0,0 +1,18 @@
+import mysql.connector
+from instance.config import MYSQL_ROOT_PASSWORD, MYSQL_ROOT_USER, MYSQL_HOST, MYSQL_DATABASE
+
+config = {
+    "user": MYSQL_ROOT_USER,
+    "password": MYSQL_ROOT_PASSWORD,
+    "database": MYSQL_DATABASE,
+    "host": MYSQL_HOST
+}
+
+
+def connect(config=config):
+    try:
+        conn = mysql.connector.connect(**config)
+    except:
+        conn = None
+
+    return conn
